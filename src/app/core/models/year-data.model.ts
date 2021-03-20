c6f7ea2fb35675404c7fab85e72ce6e5068bdd47
Nuryagdy Mustapayev <ce.nuryagdy@gmail.com>
2021-03-20 22:42:03 +0300

    fix tslint errors in app.module.ts, year-data.model.ts and parameters.service.ts

--- src/app/core/models/year-data.model.ts
+++ src/app/core/models/year-data.model.ts
@@ -2,15 +2,17 @@ export class TaxSliceModel {
     rate: number;
     ceil: number;
 }
+
 export class DisabledMonthlyIncomeTaxDiscountBaseModel {
     degree: number;
     amount: number;
 }
+
 export class YearDataModel {
 
-	year: number;
+    year: number;
     minGrossWage: number;
     SGKCeil: number;
     taxSlices: TaxSliceModel[];
-    disabledMonthlyIncomeTaxDiscountBases: DisabledMonthlyIncomeTaxDiscountBaseModel[];    
+    disabledMonthlyIncomeTaxDiscountBases: DisabledMonthlyIncomeTaxDiscountBaseModel[];
 }
--- src/app/core/services/parameters.service.ts
+++ src/app/core/services/parameters.service.ts
@@ -1,79 +1,80 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { YearDataModel, TaxSliceModel, DisabledMonthlyIncomeTaxDiscountBaseModel } from '../models/year-data.model';
-import { environment } from 'src/environments/environment';
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+import {YearDataModel, TaxSliceModel, DisabledMonthlyIncomeTaxDiscountBaseModel} from "../models/year-data.model";
+import {environment} from "src/environments/environment";
 
 interface TaxSliceResponse {
-  rate: number;
-  ceil: number;
+    rate: number;
+    ceil: number;
 }
 
 interface DisabledMonthlyIncomeTaxDiscountResponse {
-  degree: number;
-  amount: number;
+    degree: number;
+    amount: number;
 }
 
-interface yearParameterResponse {
-  year: number;
-  minGrossWage: number;
-  taxSlices: TaxSliceResponse[];
-  disabledMonthlyIncomeTaxDiscountBases: DisabledMonthlyIncomeTaxDiscountResponse[];
-  SGKCeil: number;
+interface YearParameterResponse {
+    year: number;
+    minGrossWage: number;
+    taxSlices: TaxSliceResponse[];
+    disabledMonthlyIncomeTaxDiscountBases: DisabledMonthlyIncomeTaxDiscountResponse[];
+    SGKCeil: number;
 }
 
-interface AllParametersResponse {
-  MONTHS: string[],
-  CALCULATION_CONSTANTS: CalculationConstants;
-  EMPLOYEE_EDUCATION_TYPES: EmployeeEducationTypes;
-  DISABILITY_OPTIONS: DisabilityOptions;
-  EMPLOYEE_TYPES: EmployeeTypes;
-  AGI_OPTIONS: AGIOptions;
+export interface AllParametersResponse {
+    MONTHS: string[];
+    CALCULATION_CONSTANTS: CalculationConstants;
+    EMPLOYEE_EDUCATION_TYPES: EmployeeEducationTypes;
+    DISABILITY_OPTIONS: DisabilityOptions;
+    EMPLOYEE_TYPES: EmployeeTypes;
+    AGI_OPTIONS: AGIOptions;
 
 }
-export interface CalculationConstants{
-  monthDayCount: number;
-  stampTaxRate: number;
-  employee: {
-      SGKDeductionRate: number;
-      SGDPDeductionRate: number;
-      unemploymentInsuranceRate: number;
-      pensionerUnemploymentInsuranceRate: number;
-  };
-  employer: {
-      SGKDeductionRate: number;
-      SGDPDeductionRate: number;
-      employerDiscount5746: number;
-      unemploymentInsuranceRate: number;
-      pensionerUnemploymentInsuranceRate: number;
-      SGK5746AdditionalDiscount: number;
-  };
+
+export interface CalculationConstants {
+    monthDayCount: number;
+    stampTaxRate: number;
+    employee: {
+        SGKDeductionRate: number;
+        SGDPDeductionRate: number;
+        unemploymentInsuranceRate: number;
+        pensionerUnemploymentInsuranceRate: number;
+    };
+    employer: {
+        SGKDeductionRate: number;
+        SGDPDeductionRate: number;
+        employerDiscount5746: number;
+        unemploymentInsuranceRate: number;
+        pensionerUnemploymentInsuranceRate: number;
+        SGK5746AdditionalDiscount: number;
+    };
 }
 
 export interface EmployeeEducationTypes {
-  labelText: string;
-  options:
-      {
-          id: number;
-          text: string;
-          exemptionRate: number;
-      }[]
+    labelText: string;
+    options:
+        {
+            id: number;
+            text: string;
+            exemptionRate: number;
+        }[];
 }
 
 export interface DisabilityOptions {
-  labelText: string;
-  options:
-      {
-          id: number;
-          degree: number;
-          text: string;
-      }[]
+    labelText: string;
+    options:
+        {
+            id: number;
+            degree: number;
+            text: string;
+        }[];
 }
 
 export interface EmployeeTypes {
-  labelText: string;
-  options: EmployeeType[];
+    labelText: string;
+    options: EmployeeType[];
 }
 
 export interface EmployeeType {
@@ -97,70 +98,70 @@ export interface EmployeeType {
 }
 
 export interface AGIOptions {
-  labelText: string;
-  options:
-      {
-          id: number;
-          text: string;
-          rate: number;
-      }[]
+    labelText: string;
+    options:
+        {
+            id: number;
+            text: string;
+            rate: number;
+        }[];
 }
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root"
 })
 export class ParametersService {
 
-  constructor(private http: HttpClient) {
+    constructor(private http: HttpClient) {
 
-  }
+    }
 
-  get allParameters(): Observable<AllParametersResponse>{
-    return this.http.get<AllParametersResponse>(environment.baseURL + 'assets/fixtures.json');
-  }
+    get allParameters(): Observable<AllParametersResponse> {
+        return this.http.get<AllParametersResponse>(environment.baseURL + "assets/fixtures.json");
+    }
 
-  get yearParameters(): Observable<any>{
+    get yearParameters(): Observable<any> {
 
-    return this.http.get<{yearParameters: yearParameterResponse[]}>(environment.baseURL + 'assets/year-parameters.json').pipe(
-      map(response => {
+        return this.http.get<{ yearParameters: YearParameterResponse[] }>(environment.baseURL + "assets/year-parameters.json").pipe(
+            map(response => {
 
-        const params: YearDataModel[] = [];
-        response.yearParameters.forEach(item => {
-          const newParam = new YearDataModel();
-          newParam.year = item.year;
-          newParam.minGrossWage = item.minGrossWage;
-          newParam.SGKCeil = item.SGKCeil;
+                const params: YearDataModel[] = [];
+                response.yearParameters.forEach(item => {
+                    const newParam = new YearDataModel();
+                    newParam.year = item.year;
+                    newParam.minGrossWage = item.minGrossWage;
+                    newParam.SGKCeil = item.SGKCeil;
 
-          newParam.taxSlices = [];
-          item.taxSlices.forEach(it => {
-            const taxSlice = new TaxSliceModel();
-            taxSlice.rate = it.rate;
-            taxSlice.ceil = it.ceil;
-            newParam.taxSlices.push(taxSlice);
-          });
+                    newParam.taxSlices = [];
+                    item.taxSlices.forEach(it => {
+                        const taxSlice = new TaxSliceModel();
+                        taxSlice.rate = it.rate;
+                        taxSlice.ceil = it.ceil;
+                        newParam.taxSlices.push(taxSlice);
+                    });
 
-          newParam.disabledMonthlyIncomeTaxDiscountBases = [];
-          item.disabledMonthlyIncomeTaxDiscountBases.forEach(it => {
-            const base = new DisabledMonthlyIncomeTaxDiscountBaseModel();
-            base.degree = it.degree;
-            base.amount = it.amount;
-            newParam.disabledMonthlyIncomeTaxDiscountBases.push(base);
-          });
+                    newParam.disabledMonthlyIncomeTaxDiscountBases = [];
+                    item.disabledMonthlyIncomeTaxDiscountBases.forEach(it => {
+                        const base = new DisabledMonthlyIncomeTaxDiscountBaseModel();
+                        base.degree = it.degree;
+                        base.amount = it.amount;
+                        newParam.disabledMonthlyIncomeTaxDiscountBases.push(base);
+                    });
 
-          params.push(newParam);
-        });
-        return params;
-      }),
-      map(params => {
-        //sort in descending order by year
-        params.sort((a, b) => b.year - a.year);
-        //sort tax slices in ascending order
-        params.forEach(item => {
-          item.taxSlices.sort((a, b) => a.rate - b.rate);
-        });
-        return params;
-      })
-    );
-  }
+                    params.push(newParam);
+                });
+                return params;
+            }),
+            map(params => {
+                // sort in descending order by year
+                params.sort((a, b) => b.year - a.year);
+                // sort tax slices in ascending order
+                params.forEach(item => {
+                    item.taxSlices.sort((a, b) => a.rate - b.rate);
+                });
+                return params;
+            })
+        );
+    }
 }
